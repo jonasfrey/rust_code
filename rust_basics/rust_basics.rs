368767471eb2e190dcc5751e0ceebc4fab3dc93d
jf18j492 <jonas.frey@csh.unibe.ch>
2022-05-26 01:04:57 +0200

    rust hust(le)

--- rust_basics/rust_basics.rs
+++ rust_basics/rust_basics.rs
@@ -43,27 +43,14 @@ macro_rules! f_create_function_by_identifier {
         }
     }
 }
-fn eval_dynamic(st:&str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    std::fs::write("/tmp/eval.rs",format!("#[no_mangle]fn eval() -> Vec<u8> {{ { }}}",st))?;
-    ::std::process::Command::new("rustc")
-        .args(&[
-            "-o","/tmp/eval.so",
-            "--crate-type","dylib",
-            "/tmp/eval.rs"
-        ]).spawn()?.wait_with_output()?;
-    unsafe {
-        let lib = libloading::Library::new("/tmp/eval.so")?;
-        let func: libloading::Symbol<unsafe extern fn() -> Vec<u8>> = lib.get(b"eval")?;
-        Ok(func())
-    }
-}
+
 macro_rules! f_create_function_by_identifier_and_expression {
 
     // ($s_function_name:expr) => { //this is not working we have to use "ident" (identifier)
     ($i_function_name:ident, $s_function_body:literal) => {
         // creating a new function 
         fn $i_function_name(){
-            let evaluated_result = eval_dynamic($s_function_body);
+            let evaluated_result = eval($s_function_body);
             println!("evaluated_result is : {}", evaluated_result);
             println!("function created by f_create_function was called!");
         }
@@ -100,9 +87,123 @@ fn main() {
 
     f_create_function_by_identifier!(f_test); // f_test is now a identifier! 
     f_test();
-    f_create_function_by_identifier_and_expression!(f_another, "1+(1)+(1+(1))+(1+(1)+(1+(1)))+(1+(1)+(1+(1))+(1+(1)+(1+(1))))");
-    f_another();
 
+    
+    f_println_macro_tutorial();
+
+    // the following is not working, in rust we cannot convert runtime values such a strings to a function
+    // f_create_function_by_identifier_and_expression!(f_another, "1+(1)+(1+(1))+(1+(1)+(1+(1)))+(1+(1)+(1+(1))+(1+(1)+(1+(1))))");
+    // f_another();
+
+    f_type_casting();
+    // f_address_and_value_aka_index_and_value();
+    // f_datatypes();
+
+    
+}
+
+fn f_println_macro_tutorial(){
+    let n_num_five = 5; 
+    let n_num_two = 2;
+
+    let n_for_debugging = 12341234; 
+    let s_for_debugging = "the variable s_for_debugging holds this string"; 
+    let a_for_debugging = [1,2,3,4,5];
+
+    println!("we can print a string an brakets get substituated with variables such as : {}", n_num_five);
+    
+    
+    println!("we can print multiple vars like this {}, {}", n_num_five, n_num_two);
+    
+    println!("we can set the order by adding the index in the bracket {1}, {0}", n_num_five, n_num_two);
+    
+    
+    println!("{{:?}} is for debugging, n_for_debugging: {:?}", n_for_debugging);
+    println!("{{:?}} is for debugging, s_for_debugging: {:?}", s_for_debugging);
+    // note {} wont print quotes around the string variable 
+    println!("{{}} is for printing any var, s_for_debugging: {}", s_for_debugging);
+    
+    
+    // pretty print {:#?}
+    // println!("a_for_debugging : {}", a_for_debugging); // a_for_debugging cannot be printed with {}, we need to use {:#?}
+
+    println!("{{:#?}} is for pretty print n_for_debugging: {:#?} ", n_for_debugging);
+
+    println!("{{:#?}} is for pretty print s_for_debugging: {:#?} ", s_for_debugging);
+    
+    println!("{{:#?}} is for pretty print a_for_debugging: {:#?} ", a_for_debugging);
+
+    let n_address = &n_num_five;
+
+    println!("n_address : {:?}", (&n_num_five)); // wtf ds isch ja huere schlecht wtf i schwoere wtf scheiss rust huere fcking hipster scheiss sprach , nid besser aus c wtf huere behbinderti scheisse ohni witz ey
+    println!("n_address : {:#?}", (&n_num_five)); // wttttfff wtf
+
+    // 1 und 1 = 2 und 1 = 3
+    
+    
+    // actually println! is noting more than a print of a fromated string 
+
+    let s_formated = format!("this is a formated string, n_num_five: {}", n_num_five); 
+    println!("{}", s_formated);
+
+}
+
+fn f_address_and_value_aka_index_and_value(){
+    
+    // since rust is low level, it has, as expected, address and value of any variable 
+
+    let n_num = 10; 
+
+    println!("value('n_num'):{}", n_num);
+    // println!("address('&n_num'):{}", (&n_num)); // println! wont work when we dont use {:p} 
+    println!("address('&n_num'):{:p}", &n_num);
+    
+
+    // we can also print as integer 
+    // println!("address('&n_num'):{}", ((&n_num) as i64) );
+
+    let n_address = &n_num;
+}
+// use std::char;
+fn f_type_casting(){
+
+    let n_num_ascii_a = 65; 
+    println!("n_num_ascii_a: {}", n_num_ascii_a);
+
+    let s_num_ascii_a_as_char = char::from_u32(n_num_ascii_a).unwrap();
+    println!("s_num_ascii_a_as_char: {}", s_num_ascii_a_as_char);
+
+    // println!("s_num_ascii_a_as_char: {}", n_num_ascii_a);
+
+   
+    let n_num_ascii_a: u8 = 65; 
+    println!("n_num_ascii_a: {}", n_num_ascii_a);
+
+    let s_num_ascii_a_as_char = n_num_ascii_a as char; 
+    println!("s_num_ascii_a_as_char: {}", s_num_ascii_a_as_char);
+
+    // let n_basesixteen_address_of_n_num_ascii_a: *const u64 = &n_num_ascii_a;
+    // println!("n_basesixteen_address_of_n_num_ascii_a {:p}", n_basesixteen_address_of_n_num_ascii_a);
+
+    // // let n_baseten_address_of_n_num_ascii_a = n_basesixteen_address_of_n_num_ascii_a as u64;
+    // let n_baseten_address_of_n_num_ascii_a_u64 = n_basesixteen_address_of_n_num_ascii_a as u64;
+    // println!("n_baseten_address_of_n_num_ascii_a_u64 {}", n_baseten_address_of_n_num_ascii_a_u64);
+
+    // let n_baseten_address_of_n_num_ascii_a_usize = n_basesixteen_address_of_n_num_ascii_a as usize;
+    // println!("n_baseten_address_of_n_num_ascii_a_usize {}", n_baseten_address_of_n_num_ascii_a_usize);
+
+    // println!("n_num_ascii_a: {:c}", n_num_ascii_a);
+
+}
+
+fn f_datatypes(){
+
+    // rust guesses the type by default 
+
+    // let n_num_int = 10; 
+    // println!(n_num_int);
+    // let n_num_float = 1.1235; 
+    
 }
 
 
